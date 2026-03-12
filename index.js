bfd36a27af31e1bb7f462cda62a43d75a82e7b3a
Kunal Kumar <krishnakk9091@gmail.com>
2026-03-12 08:29:05 +0000

    cors

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import {DB_NAME} from "./constants.js"
 import connectDB from "./src/db/index.js";
+import { app } from "./app.js";
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`app running on port${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed",err)
+})
